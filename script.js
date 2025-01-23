0195c1e14d117c2d805f5ff9a5044d6f8f3c768c
ig-ayush <igayushacharya@gmail.com>
2025-01-24 00:16:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const navbar = document.querySelector('.navbar');
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+window.addEventListener('scroll',()=>{
+    if(window.scrollY > 50){
+        navbar.classList.add('scrolled');
+    }else{
+        navbar.classList.remove('scrolled');
+    }
+});
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
